6bb8d5e6598e0cead72c88f5af7df8655068db3b
TunazZz <forwork1410@gmail.com>
2024-10-02 23:27:08 +0700

    Fix i18n

--- CaseStudy1/src/main/java/com/tun/casestudy1/config/WebConfig.java
+++ CaseStudy1/src/main/java/com/tun/casestudy1/config/WebConfig.java
@@ -3,32 +3,44 @@ package com.tun.casestudy1.config;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("lang");
-        registry.addInterceptor(localeChangeInterceptor);
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 
     @Bean
     public LocaleResolver localeResolver() {
-        return new CookieLocaleResolver();
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        Locale vietnamLocale = new Locale("vi");
+        slr.setDefaultLocale(vietnamLocale);
+        return slr;
     }
 
     @Bean("messageSource")
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("lang/messages");
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasenames("classpath:i18n/messages");
+        messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
 
